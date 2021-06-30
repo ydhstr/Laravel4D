d02bba172215e2d7ed922373223ddb0ef17951f1
ydhstr <81416846+ydhstr@users.noreply.github.com>
2021-06-29 21:08:56 -0700

    Laravel

---
+++ app/Mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table        ='mahasiswa';
+    protected $fillable     = ['user_id', 'npm', 'tempat_lahir', 'tanggal_lahir', 'alamat', 'telepon', 'gender'];
+    public $timestamps      = false;
+    
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+}
